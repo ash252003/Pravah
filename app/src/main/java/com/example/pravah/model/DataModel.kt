6fdc480ee0e667e4781831cfa1daf4a8695f2b16
ash252003 <ay67986798@google.com>
2026-08-14 17:29:55 +0530

    User Navigation

--- app/src/main/java/com/example/pravah/model/DataModel.kt
+++ app/src/main/java/com/example/pravah/model/DataModel.kt
@@ -1,6 +1,14 @@
 package com.example.pravah.model
 
+import androidx.compose.ui.graphics.vector.ImageVector
+
 data class DataModel(
     val id: String,
     val institution: String
+)
+
+data class DrawerItem(
+    val title: String,
+    val route: String,
+    val icon: ImageVector
 )
--- app/src/main/java/com/example/pravah/nav/UserNavigation.kt
+++ app/src/main/java/com/example/pravah/nav/UserNavigation.kt
@@ -1,8 +1,140 @@
 package com.example.pravah.nav
 
+import android.content.Context
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Class
+import androidx.compose.material.icons.filled.ControlCamera
+import androidx.compose.material.icons.filled.ManageAccounts
+import androidx.compose.material.icons.filled.MeetingRoom
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Sensors
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationDrawerItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.example.pravah.R
+import com.example.pravah.model.DrawerItem
+import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun UserNavigation(rootNavController: NavController) {
+    val drawerState = rememberDrawerState(DrawerValue.Closed)
+    val scope = rememberCoroutineScope()
+    val navController = rememberNavController()
+    val instituteItem = listOf(
+        DrawerItem("Device Status", "device_status", Icons.Default.Sensors),
+        DrawerItem("Monitor Occupancy", "monitor_occupancy", Icons.Default.MeetingRoom),
+        DrawerItem("Manage Classroom", "manage_classroom", Icons.Default.Class),
+        DrawerItem("Manage Staff", "manage_staff", Icons.Default.ManageAccounts),
+        DrawerItem("Control Manually", "control_manually", Icons.Default.ControlCamera)
+    )
+    val staffItem = listOf(
+        DrawerItem("Device Status", "device_status", Icons.Default.Sensors),
+        DrawerItem("Monitor Occupancy", "monitor_occupancy", Icons.Default.MeetingRoom),
+        DrawerItem("Control Manually", "control_manually", Icons.Default.ControlCamera)
+    )
+    val context = LocalContext.current
+    val sharedPreferences = context.getSharedPreferences("user_session", Context.MODE_PRIVATE)
+    val userType = sharedPreferences.getString("user_type", null)
+    val item = if (userType == "staff") staffItem else instituteItem
+    val currentRoute = navController.currentBackStackEntryAsState().value?.destination?.route
+    ModalNavigationDrawer(
+        drawerContent = {
+            ModalDrawerSheet(
+                modifier = Modifier.width(300.dp),
+            ) {
+                Column(
+                    modifier = Modifier
+                        .padding(horizontal = 16.dp)
+                        .verticalScroll(rememberScrollState())
+                ) {
+
+                    Spacer(Modifier.height(16.dp))
+                    item.forEach { item ->
+                        NavigationDrawerItem(
+                            label = { Text(item.title) },
+                            selected = currentRoute == item.route,
+                            onClick = {
+                                navController.navigate(item.route){
+                                    popUpTo("user_home")
+                                    launchSingleTop = true
+                                }
+                                scope.launch {
+                                    drawerState.close()
+                                }
+                            },
+                            icon = {
+                                Icon(item.icon, contentDescription = "Menu Item")
+                            },
+                        )
+                    }
+                }
+            }
+        },
+        drawerState = drawerState
+    ) {
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    title = { Text("CourtInsight") },
+                    navigationIcon = {
+                        IconButton(onClick = {
+                            scope.launch {
+                                if (drawerState.isClosed) {
+                                    drawerState.open()
+                                } else {
+                                    drawerState.close()
+                                }
+                            }
+                        }) {
+                            Icon(
+                                Icons.Default.Menu,
+                                contentDescription = "Menu",
+                                tint = colorResource(R.color.white)
+                            )
+                        }
+                    },
+                    colors = topAppBarColors(
+                        containerColor = MaterialTheme.colorScheme.primary,
+                        titleContentColor = colorResource(R.color.white)
+                    )
+                )
+            }
+        ) { innerPadding ->
+            NavHost(
+                navController = navController,
+                startDestination = "home",
+                modifier = Modifier.padding(innerPadding)
+            ) {
+            }
+        }
+    }
 }
